398059fcf907f7ac16dc9da9c8e29941d6722c99
Saulius <60930326+Saulius1962@users.noreply.github.com>
2020-05-10 17:30:35 +0300

    Initial commit

---
+++ src/main/java/lt/ss/java2/model/Client.java
@@ -0,0 +1,32 @@
+package lt.ss.java2.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "klientai")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 100)
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
